e43677cc88b8d2d4e38a4349302cbf35deebfbd2
Ryo <kanadajinlee@yahoo.ca>
2017-01-14 14:15:41 -0500

    Worked on basics for the main page

---
+++ front-end/src/profile-pic.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class ProfilePic extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <img className="profile-pic" src="https://www.iconexperience.com/_img/o_collection_png/green_dark_grey/512x512/plain/dog.png"/>
+    )
+  }
+}
+
+export default ProfilePic;
